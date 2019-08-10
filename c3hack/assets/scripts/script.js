b901a12be683b1f1e2eaed1dee56634005e9e3c0
Jorge Siqueira <jorge.siqueira@furg.br>
2019-08-09 23:12:52 -0300

    version 3.0

--- c3hack/assets/scripts/script.js
+++ c3hack/assets/scripts/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(document).ready(function onDocumentReady() {
   var animation = bodymovin.loadAnimation({
     container: document.getElementById('illustration'),
@@ -68,75 +67,4 @@ function gtag() {
   dataLayer.push(arguments);
 }
 gtag('js', new Date());
-=======
-$(document).ready(function onDocumentReady() {
-  var animation = bodymovin.loadAnimation({
-    container: document.getElementById('illustration'),
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: 'assets/json/data.json'
-  });
-});
-window.dataLayer = window.dataLayer || [];
-
-$('#menuSAICC').hover(
-  function () {
-    $(this).addClass('active')
-  },
-  function () {
-    $(this).removeClass('active')
-  }
-)
-
-$('#menuCronograma').hover(
-  function () {
-    $(this).addClass('active')
-  },
-  function () {
-    $(this).removeClass('active')
-  }
-)
-
-$('#menuDesafio').hover(
-  function () {
-    $(this).addClass('active')
-  },
-  function () {
-    $(this).removeClass('active')
-  }
-)
-
-$('#menuAvaliacao').hover(
-  function () {
-    $(this).addClass('active')
-  },
-  function () {
-    $(this).removeClass('active')
-  }
-)
-
-$('#menuPremiacao').hover(
-  function () {
-    $(this).addClass('active')
-  },
-  function () {
-    $(this).removeClass('active')
-  }
-)
-
-$('#menuFAQ').hover(
-  function () {
-    $(this).addClass('active')
-  },
-  function () {
-    $(this).removeClass('active')
-  }
-)
-
-function gtag() {
-  dataLayer.push(arguments);
-}
-gtag('js', new Date());
->>>>>>> master
 gtag('config', 'UA-143516100-1');
